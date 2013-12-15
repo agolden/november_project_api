ec7beefeec7d40243a92fd6583a8de75eceae669
agolden <felix@agolden.com>
2013-12-15 14:39:43 -0500

    Add GET workout location by id

--- hidden/service_layer/NP_WorkoutLocationService.php
+++ hidden/service_layer/NP_WorkoutLocationService.php
@@ -19,5 +19,24 @@
 			}
 			return $response;
 		}
+		
+		function getLocationById($id)
+		{
+			$response = array();
+			$stmt = $this->DBH->prepare('SELECT * FROM workout_location where id = :id');
+			$stmt->execute(array(':id' => $id));
+			
+			foreach($stmt->fetchAll() as $record) {
+				$jsonObject = new WorkoutLocationModel;
+				$jsonObject->id = $record['id'];
+				$jsonObject->tribe_id = $record['tribe_id'];
+				$jsonObject->name = $record['name'];
+				$jsonObject->lat = $record['latitude'];
+				$jsonObject->lng = $record['longitude'];
+				
+				$response[] = $jsonObject->getArray();
+			}
+			return $response;
+		}
 	}
 ?>
